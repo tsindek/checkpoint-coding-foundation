8f0cca1357145e8d1245f74550f645abbd67a2a8
Tobias Sindek <tobias.sindek@gmail.com>
2022-02-17 09:16:45 +0100

    feat: add sumApp feature
    sumApp can recieve and add up numbers. reset feature also implemented

--- sum-app.js
+++ sum-app.js
@@ -1,3 +1,19 @@
 class SumApp {
-  // Add you solution here
+  constructor() {
+    this.numbers = [];
+  }
+
+  addNumber(n) {
+    this.numbers.push(n);
+  }
+
+  getSum() {
+    return this.numbers.reduce(
+      (previousValue, currentValue) => previousValue + currentValue
+    );
+  }
+
+  reset() {
+    this.numbers = [];
+  }
 }
